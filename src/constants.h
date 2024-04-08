dd6d0c48d509df5a44c8c57a6250d946f2a55ba4
Tusu <321756623tu@gmail.com>
2024-04-08 08:16:46 -0400

    add the constants.h file

---
+++ src/constants.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace Constants
+{
+    int width = 800;
+    int height = 600;
+
+    float mouseShapeRadius = 10.f;
+    float playerRadius = 20.f;
+
+    int playerSpeed = 5;
+}
+
+#endif // CONSTANTS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,95 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/Cursor.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include <iostream>
+#include "constants.h"
 
-int width = 800;
-int height = 600;
-
-float mouseShapeRadius = 10.f;
-float playerRadius = 20.f;
-
-int playerSpeed = 5;
-
-
-void player_move(bool left, bool right, bool up, bool down, float* player_x, float* player_y) {
-    if (left && up) {
-        std::cout << "Moving player diagonally up-left" << std::endl;
-        // Adjust player's position accordingly
-        *player_x -= 0.007071f * playerSpeed;
-        *player_y -= 0.007071f * playerSpeed;
-    } else if (left && down) {
-        std::cout << "Moving player diagonally down-left" << std::endl;
-        // Adjust player's position accordingly
-        *player_x -= 0.007071f * playerSpeed;
-        *player_y += 0.007071f * playerSpeed;
-    } else if (right && up) {
-        std::cout << "Moving player diagonally up-right" << std::endl;
-        // Adjust player's position accordingly
-        *player_x += 0.007071f * playerSpeed;
-        *player_y -= 0.007071f * playerSpeed;
-    } else if (right && down) {
-        std::cout << "Moving player diagonally down-right" << std::endl;
-        // Adjust player's position accordingly
-        *player_x += 0.007071f * playerSpeed;
-        *player_y += 0.007071f * playerSpeed;
-    } else if (left) {
-        std::cout << "Moving player left" << std::endl;
-        // Adjust player's position accordingly
-        *player_x -= 0.01f * playerSpeed;
-    } else if (right) {
-        std::cout << "Moving player right" << std::endl;
-        // Adjust player's position accordingly
-        *player_x += 0.01f * playerSpeed;
-    } else if (up) {
-        std::cout << "Moving player up" << std::endl;
-        // Adjust player's position accordingly
-        *player_y -= 0.01f * playerSpeed;
-    } else if (down) {
-        std::cout << "Moving player down" << std::endl;
-        // Adjust player's position accordingly
-        *player_y += 0.01f * playerSpeed;
+void player_move(bool left, bool right, bool up, bool down, float *player_x, float *player_y)
+{
+    if (left && up)
+    {
+        *player_x -= 0.007071f * Constants::playerSpeed;
+        *player_y -= 0.007071f * Constants::playerSpeed;
+    }
+    else if (left && down)
+    {
+        *player_x -= 0.007071f * Constants::playerSpeed;
+        *player_y += 0.007071f * Constants::playerSpeed;
+    }
+    else if (right && up)
+    {
+        *player_x += 0.007071f * Constants::playerSpeed;
+        *player_y -= 0.007071f * Constants::playerSpeed;
+    }
+    else if (right && down)
+    {
+        *player_x += 0.007071f * Constants::playerSpeed;
+        *player_y += 0.007071f * Constants::playerSpeed;
+    }
+    else if (left)
+    {
+        *player_x -= 0.01f * Constants::playerSpeed;
+    }
+    else if (right)
+    {
+        *player_x += 0.01f * Constants::playerSpeed;
+    }
+    else if (up)
+    {
+        *player_y -= 0.01f * Constants::playerSpeed;
+    }
+    else if (down)
+    {
+        *player_y += 0.01f * Constants::playerSpeed;
     }
 }
 
+void player_check_move(float *player_x, float *player_y)
+{
+    bool left = false;
+    bool right = false;
+    bool up = false;
+    bool down = false;
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) or sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+    {
+        left = true;
+        std::cout << "the Left key was pressed" << std::endl;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) or sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+    {
+        right = true;
+        std::cout << "the Right key was pressed" << std::endl;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) or sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+    {
+        up = true;
+        std::cout << "the Up key was pressed" << std::endl;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) or sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+    {
+        down = true;
+        std::cout << "the Down key was pressed" << std::endl;
+    }
+
+    player_move(left, right, up, down, player_x, player_y);
+}
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(width, height), "My window");
+    sf::RenderWindow window(sf::VideoMode(Constants::width, Constants::height), "My window");
     window.setMouseCursorVisible(false);
 
-    sf::CircleShape player(playerRadius);
-    float player_x = float(width/2) - playerRadius;
-    float player_y =  float(height/2) - playerRadius;
-    player.setPosition(player_x , player_y);
+    sf::CircleShape player(Constants::playerRadius);
+    float player_x = float(Constants::width / 2) - Constants::playerRadius;
+    float player_y = float(Constants::height / 2) - Constants::playerRadius;
+
+    player.setPosition(player_x, player_y);
     player.setFillColor(sf::Color(0, 0, 0));
 
-    // run the program as long as the window is open
     while (window.isOpen())
     {
-         // draw everything here...
-        // window.draw(...);
-        
-
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
@@ -90,57 +109,29 @@ int main()
             }
             if (event.type == sf::Event::KeyPressed)
             {
-                if (event.key.scancode == sf::Keyboard::Scan::Escape)
+                // The escape key was pressed
+                if (event.key.code == sf::Keyboard::Escape)
                 {
-                    std::cout << "the escape key was pressed" << std::endl;
+                    window.close();
                 }
+                std::cout << event.key.code << std::endl;
             }
         }
 
-        
-        bool left = false;
-        bool right = false;
-        bool up = false;
-        bool down = false;
-        
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        {   
-            left = true;
-            std::cout << "the Left key was pressed" << std::endl;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        {
-            right = true;
-            std::cout << "the Right key was pressed" << std::endl;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        {
-            up = true;
-            std::cout << "the Up key was pressed" << std::endl;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-        {
-            down = true;
-            std::cout << "the Down key was pressed" << std::endl;
-        }
+        player_check_move(&player_x, &player_y);
 
-        player_move(left,right,up,down,&player_x,&player_y);
-
-        // clear the window with black color
         window.clear(sf::Color::White);
 
-        sf::CircleShape mouseShape(mouseShapeRadius);
-       
-        player.setPosition(player_x , player_y);
+        sf::CircleShape mouseShape(Constants::mouseShapeRadius);
+
+        player.setPosition(player_x, player_y);
         player.setFillColor(sf::Color(0, 0, 0));
 
         // get the local mouse position (relative to a window)
         sf::Vector2i localPosition = sf::Mouse::getPosition(window); // window is a sf::Window
-        // std::cout << localPosition.x << ' ' << localPosition.y << std::endl;
-        mouseShape.setPosition(localPosition.x - mouseShapeRadius, localPosition.y - mouseShapeRadius);
-        mouseShape.setFillColor(sf::Color(0, 0, 0));
 
-        
+        mouseShape.setPosition(localPosition.x - Constants::mouseShapeRadius, localPosition.y - Constants::mouseShapeRadius);
+        mouseShape.setFillColor(sf::Color(0, 0, 0));
 
         window.draw(mouseShape);
         window.draw(player);
@@ -151,5 +142,3 @@ int main()
 
     return 0;
 }
-
-
